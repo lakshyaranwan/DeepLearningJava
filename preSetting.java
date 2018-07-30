d60e787df2d1df4c028dec52d3bfb8280277925a
Lakshya Ranwan <lakshyaranwan4@gmail.com>
2018-07-30 22:49:57 +0530

    Create preSetting.java

---
+++ preSetting.java
@@ -0,0 +1,9 @@
+public Neural_network() {
+        double w1 = Math.random();
+		double w2 = Math.random();
+		double b = Math.random();
+        initComponents();
+         w1txt.setText(""+w1);
+	    w2txt.setText(""+w2);
+	    btxt.setText(""+b); 
+    }
